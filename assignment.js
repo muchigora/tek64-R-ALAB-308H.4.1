a0cfd005d837d32054da5c71e6f7b821b0686946
Muchi Gora <muchigora@gmail.com>
2023-05-28 03:05:01 -0400

    initial commit

---
+++ assignment.js
@@ -0,0 +1,6 @@
+/******************************************************************************
+ * Copyright 2023 by Muchi-Gora Perscholas Bootcamp Project 
+ * Powered by TEKSystems                                 *
+ * R-ALAB 308H.4.1 - JavaScript Conditionals, Loops, Objects, and Arrays
+ *****************************************************************************/
+
